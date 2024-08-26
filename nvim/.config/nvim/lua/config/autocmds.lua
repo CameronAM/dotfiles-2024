ba9080e8f579482e8e086d9038c89f2d36025e3d
Cameron Morrison <cameron.am@gmail.com>
2024-08-26 16:52:02 +1000

    terraform treesitter config, and configure completion to ignore markdown files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable completion for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    require("cmp").setup.buffer({ enabled = false })
+  end,
+})
